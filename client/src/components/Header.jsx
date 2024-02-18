8c9164fe717bcc00456eafd1bbae3471237921a2
akshaykovi <akshaykovi@gmail.com>
2024-02-18 18:26:04 +0530

    create test api route

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -7,8 +7,8 @@ export default function Header() {
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
         <Link to="/">
           <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
-            <span className="text-slate-500">Real</span>
-            <span className="text-slate-700">Estate</span>
+            <span className="text-slate-500">RealEstate</span>
+            <span className="text-slate-700">Marketplace</span>
           </h1>
         </Link>
 
